94ff50bb6d48b30df141ef4643064e3e5a09688c
GMILLICAN10 <gmillica@ucsc.edu>
2020-11-19 17:40:09 -0800

    set command line args

--- httpserver-project/main.cpp
+++ httpserver-project/main.cpp
@@ -2,60 +2,76 @@
 
 #include "servermanager.h"
 
-// remove 
+// remove
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
+void setValues(int argc, char **argv, int &threadCount, bool &redundancy, char *&address, int &port, int *wasFound)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-N") == 0)
+		{
+			threadCount = atoi(argv[i + 1]);
+			wasFound[i] = 1;
+			wasFound[i + 1] = 1;
+			i++;
+		}
+		else if (strcmp(argv[i], "-r") == 0)
+		{
+			redundancy = true;
+			wasFound[i] = 1;
+		}
+	}
+
+	bool isFirstZero = true;
+	for (int i = 1; i < argc; i++)
+	{
+		if (wasFound[i] == 0 && isFirstZero)
+		{
+			address = argv[i];
+			isFirstZero = false;
+		}
+		else if(wasFound[i] == 0)
+		{
+			port = atoi(argv[i]);
+			break;
+		}
+	}
+}
 //NEED TO ACCOUNT FOR THREADING ARGUMENTS
-int main(int argc, char** argv) {
-    int threadCount;
-    bool redundancy = false;
-    char* address;
-    int port = 80;
-    if (argc < 4) {
-        // error, not enough arguments, return
-    }
-    if (argc > 6) {
-        // error, too many arguments, return
-    }
-    if (strcmp(argv[1], "-N") == 0) {
-        threadCount = atoi(argv[2]);
-        if (strcmp(argv[3],"-r") == 0) {
-            redundancy = true;
-            address = argv[4];
-            if (argc > 5) {
-                port = atoi(argv[5]);
-            }
-        } else {
-            address = argv[3];
-            if (argc > 4) {
-                port = atoi(argv[4]);
-            }
-        }
-    } else {
-        address = argv[1];
-        if (strcmp(argv[2], "-N") == 0) {
-			cout << "enters";
-            threadCount = atoi(argv[3]);
-            if (argc == 5) {
-                if (strcmp(argv[4], "-r") == 0) {
-                    redundancy = true;
-                }
-            }
-        } else {
-			cout << "enters here";
-            port = atoi(argv[2]);
-            if (strcmp(argv[3],"-N") == 0) {
-                threadCount = atoi(argv[4]);
-                if (argc == 6) {
-                    if (strcmp(argv[4], "-r") == 0) {
-                        redundancy = true;
-                    }
-                }
-            }
-        }
-    }
-    ServerManager serverManager;
-    serverManager.Setup(address, port);
-    return 0;
+int main(int argc, char **argv)
+{
+	int threadCount;
+	bool redundancy = false;
+	char *address;
+	int port = 80;
+	if (argc < 4)
+	{
+		// error, not enough arguments, return
+	}
+	if (argc > 6)
+	{
+		// error, too many arguments, return
+	}
+	if (argc == 4)
+	{
+		int wasFound[] = {1, 0, 0, 0};
+		setValues(argc, argv, threadCount, redundancy, address, port, wasFound);
+	}
+	else if (argc == 5)
+	{
+		int wasFound[] = {1, 0, 0, 0, 0};
+		setValues(argc, argv, threadCount, redundancy, address, port, wasFound);
+	}
+	else
+	{
+		int wasFound[] = {1, 0, 0, 0, 0, 0};
+		setValues(argc, argv, threadCount, redundancy, address, port, wasFound);
+	}
+
+	ServerManager serverManager;
+	serverManager.Setup(address, port);
+	return 0;
 }
